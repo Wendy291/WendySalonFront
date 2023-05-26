9d74eef05e770d41c0caab5d2f48f28b6ec87241
Wendy291 <wendy.bignon@outlook.fr>
2023-05-26 09:26:55 +0200

    front commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UtilisateurComponent } from './utilisateur/utilisateur.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'utilisateur', // localhost:4200/utilisateur
+    component:UtilisateurComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
